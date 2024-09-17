5e3677fe418837b6f140f4d5ca9954cbc955d1b8
yrdma <artuzoygor@gmail.com>
2024-09-16 21:48:41 -0300

    inicial

--- src/DoWhileExemplo.java
+++ src/DoWhileExemplo.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class DoWhileExemplo {
     public static void main(String[] args) {
+        int opcao = 0;
+        Scanner tantofaz = new Scanner(System.in);
 
+        do {
+
+            System.out.println("**Escolha**");
+            System.out.println("1 - Inserir");
+            System.out.println("2 - Excluir");
+            System.out.println("3 - Alterar");
+            System.out.println("0 - Sair");
+            opcao = tantofaz.nextInt();
+        } while (opcao != 0);
+
+        // esse simbolo significa diferente !=
     }
 }
