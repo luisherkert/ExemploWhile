a0d46658053747a5d08e1ff8e2b29fba50c261f1
yrdma <artuzoygor@gmail.com>
2024-09-16 21:35:45 -0300

    inicial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        int valor = 0;
+        int somatoral = 0;
+
+
+        while ( valor !=99 ){
+       //   System.out.println(valor); valor++; // ou colocar valor = valor +1
+
+            System.out.println("Informe o   valor:");
+            System.out.println("Informe 99 para sair.");
+            valor = sc.nextInt();
+            somatoral += valor;
+
+
+        }
+        System.out.println("Total é: " + somatoral);
+
+
     }
 }
